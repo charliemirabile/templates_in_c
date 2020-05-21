20d0789cb7426fae7933e4c3fc209217260d0dfc
charles M mirabile <charlie.mirabile@gmail.com>
2020-05-21 12:34:46 -0400

    changed example back to using an int vector instead of a float vector as it was a remnant of other testing.

--- vector_example/main.c
+++ vector_example/main.c
@@ -1,13 +1,13 @@
-#include "templates/vector.float.t.h"
+#include "templates/vector.int.t.h"
 #include <stdio.h>
 
 int main(void)
 {
-	Vector_float *myVec = vector_float_init_default();
+	Vector_int *myVec = vector_int_init_default();
 	for(int i=0; i<10; ++i)
-		vector_float_push_back(myVec,i);
-	for(size_t i=0; i < vector_float_get_size(myVec); ++i)
-		printf("%g\n",*vector_float_at(myVec,i));
-	vector_float_destroy(&myVec);
+		vector_int_push_back(myVec,i);
+	for(size_t i=0; i < vector_int_get_size(myVec); ++i)
+		printf("%d\n",*vector_int_at(myVec,i));
+	vector_int_destroy(&myVec);
 	return 0;
 }
