e2032194bc9d6843ae35614610c6b22d05f33034
charles M mirabile <charlie.mirabile@gmail.com>
2020-05-24 10:37:19 -0400

    updated the example code to show off the new vector of pointers. Now we make a vector of integers as well as a vector of pointers to dynamically allocated integers. (this is a slightly dumb example, but it demonstrates that the vector does work and could work for any sort of dynamically allocated object.)

--- vector_and_comparison_example/main.c
+++ vector_and_comparison_example/main.c
@@ -1,5 +1,6 @@
 #include "templates/vector.int@TYPE.t.h"
 #include "templates/less.int@T.float@S.t.h"
+#include "templates/vector%.int@TYPE.t.h"
 #include <stdio.h>
 
 int main(void)
@@ -10,6 +11,18 @@ int main(void)
 	for(size_t i=0; i < vector_int_get_size(myVec); ++i)
 		printf("%d\n",*vector_int_at(myVec,i));
 	vector_int_destroy(&myVec);
+
+	Vector_p_int *myPVec = vector_p_int_init_default(NULL);
+	int *pInt = NULL;
+	for(int i=0; i<10; ++i)
+	{
+		pInt = malloc(sizeof(int));
+		*pInt = i;
+		vector_p_int_push_back(myPVec,&pInt);
+	}
+	for(size_t i=0; i < vector_p_int_get_size(myPVec); ++i)
+		printf("%d\n",*vector_p_int_at(myPVec,i));
+	vector_p_int_destroy(&myPVec);
 
 	puts(less_int_float(1,0.5f)?"less":"not less");
 	puts(less_int_float(0,0.5f)?"less":"not less");
